c7808409af80bb4c8533e534209081dcfb185266
vincent anjiri <vincentanjiri12@gmail.com>
2024-10-12 15:23:20 +0300

    Update multiple components and styles in the React application, including Brand, Feature, Features, Header, WhatGPT3, and index.css files.

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./brand.css";
+import { atlassian, dropbox, google, shopify, slack } from "./imports";
 
 function Brand() {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <img src={google} alt="google" />
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+    </div>
+  );
 }
 
 export default Brand;
---
+++ src/components/brand/imports.js
@@ -0,0 +1,7 @@
+import google from "../../assests/google.png";
+import slack from "../../assests/slack.png";
+import shopify from "../../assests/shopify.png";
+import atlassian from "../../assests/atlassian.png";
+import dropbox from "../../assests/dropbox.png";
+
+export { google, shopify, slack, atlassian, dropbox };
